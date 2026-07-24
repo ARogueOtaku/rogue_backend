9d857e856f87d453e399650f9795dca9394675c7
ARogueOtaku <amit.rockinad.das@gmail.com>
2026-07-24 21:40:45 +0530

    Server Config Setup and delete env.d.ts

--- src/cache/client.ts
+++ src/cache/client.ts
@@ -1,5 +1,6 @@
+import serverConfig from "@core/serverConfig";
 import { RedisClient } from "bun";
 
-const cacheClient = new RedisClient(process.env.CACHE_URL);
+const cacheClient = new RedisClient(serverConfig.CACHE_URL);
 
 export default cacheClient;
