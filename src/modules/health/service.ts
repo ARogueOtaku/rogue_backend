a4f159a346eb9f337c0c30fc0b0c06173e24761f
ARogueOtaku <amit.rockinad.das@gmail.com>
2026-07-26 20:56:20 +0530

    Format: apply biome formatting to env.d.ts and health service

--- src/modules/health/service.ts
+++ src/modules/health/service.ts
@@ -9,9 +9,10 @@ const HealthService = {
 	},
 	readiness: async () => {
 		try {
-      await dbClient.execute(sql`SELECT 1`);
-      const cacheResponse = await cacheClient.send("PING", []);
-      if (`${cacheResponse}`.toUpperCase() !== "PONG") throw new Error("Cache Service did not respond with 'PONG'")
+			await dbClient.execute(sql`SELECT 1`);
+			const cacheResponse = await cacheClient.send("PING", []);
+			if (`${cacheResponse}`.toUpperCase() !== "PONG")
+				throw new Error("Cache Service did not respond with 'PONG'");
 			return status(200, "🥷 Rogue Database and Cache is Available!");
 		} catch (e) {
 			console.error(e);
